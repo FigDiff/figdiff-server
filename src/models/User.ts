3633e2ea41d499497307674e2ce77c16f1192a8b
pieceofcakey <choi_927@naver.com>
2024-07-26 20:15:59 +0900

    Fix: Add default values to schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -19,17 +19,17 @@ interface User extends Document {
 const historySchema: Schema<History> = new Schema({
   imageId: { type: String, required: true },
   imageUrl: { type: String, required: true },
-  date: { type: Date, required: true },
+  date: { type: Date, default: Date.now(), required: true },
 });
 
 const tabUrlSchema: Schema<TabUrl> = new Schema({
   url: { type: String, required: true },
-  history: { type: [historySchema], required: true },
+  history: { type: [historySchema], default: [], required: true },
 });
 
 const userSchema: Schema<User> = new Schema({
   userId: { type: String, required: true },
-  tabUrl: { type: [tabUrlSchema], required: true },
+  tabUrl: { type: [tabUrlSchema], default: [], required: true },
 });
 
 const User: Model<User> = model<User>("User", userSchema);
