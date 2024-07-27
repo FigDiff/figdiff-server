f33bc7e2ae31c95f8982392c826f29d0501d0935
정든결 <wantchurros@jeongdeungyeol-ui-MacBookAir.local>
2024-07-27 17:05:22 +0900

    Feat: Add ProgressBar routes

--- src/loaders/routers.ts
+++ src/loaders/routers.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import figmaDataRouter from "../routes/figmaData";
+import progressSSE from "../routes/progressBarSSE";
 
 const routerLoader = async (app: Express) => {
   app.use("/", figmaDataRouter);
+  app.use("/", progressSSE);
 };
 
 export default routerLoader;
