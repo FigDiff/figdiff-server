1972a8e494c7038f87d4f7f868592813ff08d9f8
pieceofcakey <choi_927@naver.com>
2024-07-30 01:19:15 +0900

    Feat: Add historyRouter

--- src/loaders/routers.ts
+++ src/loaders/routers.ts
@@ -1,10 +1,12 @@
 import { Express } from "express";
 import figmaDataRouter from "../routes/figmaData";
 import progressSSE from "../routes/progressBarSSE";
+import historyRouter from "../routes/history";
 
 const routerLoader = async (app: Express) => {
   app.use("/", figmaDataRouter);
   app.use("/", progressSSE);
+  app.use("/", historyRouter);
 };
 
 export default routerLoader;
