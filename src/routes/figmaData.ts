a88e3e13f70a4c3928c7b0dd287abba2a3d7ceb9
pieceofcakey <choi_927@naver.com>
2024-07-30 01:16:25 +0900

    Fix: Remove unnecessary multer module at figmaData router

--- src/routes/figmaData.ts
+++ src/routes/figmaData.ts
@@ -1,13 +1,9 @@
 import express from "express";
-import multer from "multer";
 
 import figmaDataController from "../controllers/figmaData.controller";
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
 const router = express.Router();
 
-router.post("/figma-data", upload.single("screenshot"), figmaDataController);
+router.post("/figma-data", figmaDataController);
 
 export default router;
